e090d166844c251c7c5b270af4695387159914ca
nerdboi123 <e1223lymm@naver.com>
2025-08-05 10:41:12 +0900

    배포 테스트 1차

--- src/main/java/com/next/app/api/user/controller/HelloController.java
+++ src/main/java/com/next/app/api/user/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@Tag(name = "Hello Controller", description = "간단한 테스트 API")
+@Tag(name = "Hello Controller", description = "배포 테스트 중입니다 안녕하세요!!!")
 public class HelloController {
 
     @GetMapping("/hello")
